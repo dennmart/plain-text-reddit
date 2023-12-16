0866c7a3c2baf1b64af780ca91c247598288fa52
Dennis Martinez <dennis@dennmart.com>
2023-12-16 16:52:17 +0900

    Initial pass at rendering top posts of the week for a subreddit

---
+++ app/r/[slug]/page.tsx
@@ -0,0 +1,70 @@
+import type { Metadata } from "next";
+
+type PostData = {
+  id: string;
+  title: string;
+  selftext: string;
+};
+
+type RedditResponse = {
+  data: {
+    children: {
+      data: PostData;
+    }[];
+  };
+};
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  return {
+    title: `Plain Text Reddit: Top posts of the week for r/${params.slug}`,
+  };
+}
+
+async function getTopSubredditPosts(subreddit: string): Promise<PostData[]> {
+  const res = await fetch(
+    `https://www.reddit.com/r/${subreddit}/top.json?t=week&limit=50`,
+    {
+      headers: {
+        "User-Agent": "Plain Text Reddit/0.1",
+      },
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch posts");
+  }
+
+  const posts: RedditResponse = await res.json();
+
+  return posts.data.children.map((post) => {
+    const { id, title, selftext } = post.data;
+
+    return {
+      id,
+      title,
+      selftext,
+    };
+  });
+}
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const posts = await getTopSubredditPosts(params.slug);
+
+  return (
+    <div className="mt-4 space-y-8">
+      <h2 className="text-center text-xl font-bold">
+        Top posts of the week for r/{params.slug}
+      </h2>
+      {posts.map((post) => (
+        <section key={post.id}>
+          <div className="font-bold">Title: {post.title}</div>
+          <p className="mt-1">{post.selftext}</p>
+        </section>
+      ))}
+    </div>
+  );
+}
